bd4711da3403624b33a33f0d6de0e2ae1c5fdc2f
Arthur de Paula Correa <arthurdepaulacorrea@hotmail.com>
2024-09-06 19:44:47 -0300

    Setup : Google Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      caesarDressing: ["Caesar Dressing", "system-ui"],
+      chathura: ["Chathura", "system-ui"],
+    },
     extend: {},
   },
   plugins: [],
